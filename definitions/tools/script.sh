d09288ec1f6601e5835255b6e18c4d50c6e94635
Aljorani <layth@compute1-client-1.ris.wustl.edu>
2022-10-29 15:43:12 -0500

    removing script that was added by accident

--- definitions/tools/script.sh
+++
@@ -1,2 +0,0 @@
-sed -i'' -e 's/runtime {\n    preemptible: 1\n    maxRetries: 2/n    preemptible: 1\n    maxRetries: 2/n    preemptible: 1\n    maxRetries: 2/runtime {\n    preemptible: 1\n    maxRetries: 2/g' *
-
